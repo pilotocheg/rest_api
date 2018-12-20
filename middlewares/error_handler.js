28198c7dc4ebec08380a017fd4880433fd78c7c7
pilotocheg <slavutich.org@gmail.com>
2018-12-20 07:10:46 +0200

    fix start script and errorHandler

--- middlewares/error_handler.js
+++ middlewares/error_handler.js
@@ -1,3 +1,7 @@
+const Debug = require("debug");
+
+const debug = Debug("test_api");
+
 module.exports = (err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
@@ -5,5 +9,6 @@ module.exports = (err, req, res, next) => {
   if (typeof err === "string") {
     res.status(404).send(err);
   }
+  debug(err);
   res.status(err.status || 500).send(err);
 }
